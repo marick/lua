b1cbd8b992e232f537263b8c0341df6a41ffe36c
Brian Marick <marick@exampler.com>
2018-08-20 16:21:07 -0500

    Copied the solution

--- rv.sh
+++ rv.sh
@@ -1,26 +1,23 @@
-#!/bin/bash
+#!/bin/sh
+set -x # print debug info
+
 # This is running under Ubuntu 16.04
-# Install necessary packages.
-# sudo apt-get install blablabla -y
+# Install necessary packages. 
+sudo apt-get install -y libreadline-dev
 
-# Replace your C compiler command like `gcc` with `kcc`,
-# Use `-fissue-report` flag to collect errors to `my_errors.json` file.
-kcc -fissue-report=./my_errors.json main.c -o a.out
+# Compile and run `lua`.
+error_file=`pwd`/my_errors.json
+compiler=kcc
+sudo make -j`nproc` CC=$compiler LD=$compiler CFLAGS=-fissue-report=$error_file 
+rm $error_file
 
-# Run the compiled program and collect run-time errors to `my_errors.json` file, which
-# will be used next step to generate HTML report.
-./a.out
+`pwd`/lua -e "print(\"Hello, World!\")"
 
-if [ -f ./my_errors.json ]; then
-  # Generate HTML report with `rv-html-report` command,
-  # and output the HTML report to `./report` directory.
-  rv-html-report ./my_errors.json -o report
+# Generate a HTML report with `rv-html-report` command,
+# and output the HTML report to `./report` directory. 
+rv-html-report $error_file -o report
 
-  # Upload your HTML report to RV-Toolkit website with `rv-upload-report` command.
-  # Please note that `rv-upload-report` only accepts absolute path of HTML report.
-  rv-upload-report `pwd`/report
+# Upload your HTML report to RV-Toolkit website with `rv-upload-report` command. 
+rv-upload-report `pwd`/report
 
-else
-  echo "No errors found"
-fi
 # Done.
